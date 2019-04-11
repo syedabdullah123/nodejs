34bf71df067f00a754ded7f7508162d184005e81
syedabdullah123 <43432605+syedabdullah123@users.noreply.github.com>
2019-04-12 03:55:58 +0530

    changed

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
     'mongodb+srv://abdullah:HNbBQDgV3jqywxaF@cluster0-cdqxx.mongodb.net/test?retryWrites=true'
-  )
+    )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
